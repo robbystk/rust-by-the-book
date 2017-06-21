eca338bf6c8fc275dfb218c71df41163e0015b03
robby <robbystk@gmail.com>
2017-06-20 21:19:24 -0600

    Generate random number

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ use rand::Rng;
 fn main() {
     println!("Guess the number!");
 
+    let secret_number = rand::thread_rng().gen_range(1,101);
+
+    println!("The secret number is: {}", secret_number);
+
     println!("Please input your guess.");
 
     let mut guess = String::new();
