29e6951ed0e56a691b89da3e4e5eaef47c28c435
robby <robbystk@gmail.com>
2017-07-07 20:44:32 -0600

    Variable shadowing is different than mutation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let mut x = 5;
-    const MAX_POINTS: u32 = 100_000;
-    println!("The value of MAX_POINTS is: {}", MAX_POINTS);
+    let x = 5;
     println!("The value of x is: {}", x);
-    x = 6;
+    let x = x + 1;
+    println!("The value of x is: {}", x);
+    let x = x * 2;
     println!("The value of x is: {}", x);
 }
