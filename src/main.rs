e31e0f1e28c13e005487d8112b0f243047791413
robby <robbystk@gmail.com>
2017-07-07 22:18:06 -0600

    Function declaration

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 fn main() {
-    // Array (allocated on stack) (fixed size)
-    let a = [1, 2, 3, 4, 5];
-    let index = 10;
+    println!("Hello, world!");
 
-    let third = a[2];
+    another_function();
+}
 
-    println!("The third element of a is: {}", third);
-
-    // runtime error with out of bound index
-    println!("The eleventh element f a is: {}", a[index]);
+// function declaration
+// names in snake_case
+fn another_function() {
+    println!("Another function.");
 }
