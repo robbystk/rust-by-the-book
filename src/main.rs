cfc6f04eee0ccba904302600bc7e8f49910a326c
robby <robbystk@gmail.com>
2017-07-07 20:47:52 -0600

    Shadowing allows change of type
    mut does not allow type changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     let x = 5;
     println!("The value of x is: {}", x);
@@ -5,4 +7,14 @@ fn main() {
     println!("The value of x is: {}", x);
     let x = x * 2;
     println!("The value of x is: {}", x);
+
+    // Get a number of spaces from user
+    println!("Enter your desired number of spaces");
+    let mut spaces = String::new();
+    io::stdin().read_line(&mut spaces)
+        .expect("Failed to read line");
+
+    let spaces = spaces.len();
+
+    println!("Number of spaces: {}", spaces);
 }
