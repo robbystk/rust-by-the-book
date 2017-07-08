058d5ab62c5447d75ef873c2049d5cb085d1ef9b
robby <robbystk@gmail.com>
2017-07-07 22:20:11 -0600

    Function arguments

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 fn main() {
-    println!("Hello, world!");
-
-    another_function();
+    another_function(5, 6);
 }
 
 // function declaration
 // names in snake_case
-fn another_function() {
-    println!("Another function.");
+fn another_function(x: i32, y: i32) {
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
 }
