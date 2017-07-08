c44b260a6ea6192558bff9908d2b25bef8992f5b
robby <robbystk@gmail.com>
2017-07-07 20:30:19 -0600

    Constants with 'const'
    They must have types
    Also can put underscores in numbers to improve legibility
    e.g.: 100_000_000_000 instead of 100000000000

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
     let mut x = 5;
+    const MAX_POINTS: u32 = 100_000;
+    println!("The value of MAX_POINTS is: {}", MAX_POINTS);
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
