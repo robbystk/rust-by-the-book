702aab3044795820e989f7da56fd9c119b798228
robby <robbystk@gmail.com>
2017-07-07 21:03:50 -0600

    Integer types

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
-    let guess: u32 = "42".parse().expect("Not a number!");
-
-    // compile-time error:
-    // let guess = "42".parse().expect("Not a number!");
+    let a: i64 = 98_222;
+    let b: u32 = 0xff;
+    let c: u8 = 0o77;
+    let d: u16 = 0b0101_1001;
+    let e: u8 = b'A';
 }
