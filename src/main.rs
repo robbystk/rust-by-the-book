9e6326072e7b38266b2d1023edbb7acb2768d4b2
robby <robbystk@gmail.com>
2017-07-29 14:11:43 -0600

    You must specifically ask for comparison to zero

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 fn main() {
     let number = 7;
 
-    if number {
-        println!("condition was true");
-    } else {
-        println!("condition was false");
+    if number != 0 {
+        println!("number was suomething other than zero");
     }
 }
