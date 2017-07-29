60cb3549ce9b593a28a5c56e6aa81ade8c6c2f76
robby <robbystk@gmail.com>
2017-07-29 13:49:49 -0600

    Function with return value

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,9 @@
 fn main() {
-    let y = 6;  // this is a statement (no return)
-    // no worky:
-    // let wat = (let y = 6);
+    let x = five();
 
-    y + 1;  // y + 1 is an expression (returns)
-
-    // RHS of this is an expression that evaluates to 4
-    let thing = {
-        let x = 3;
-        x + 1
-    }
-
-    // a semicolon turns an expression into a statement
-
-    another_function(5, 6); // also an expression (except semicolon)
+    println!("The value of x is: {}", x);
 }
 
-// function definition is a statement
-fn another_function(x: i32, y: i32) {
-    println!("The value of x is: {}", x);   // expression (except semicolon)
-    println!("The value of y is: {}", y);
+fn five() -> i32 {
+    5
 }
