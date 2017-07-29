d1dfd87cfd1ab5329570a40988fe79a1a617cd3e
robby <robbystk@gmail.com>
2017-07-29 16:02:33 -0600

    For loop over values in a range

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
-    let a = [10, 20, 30, 40, 50];
-
-    for element in a.iter() {
-        println!("the value is: {}", element);
+    // count down from 4
+    // the rev() method reverses the range
+    for number in (1..4).rev() { 
+        println!("{}!", number);
     }
+    println!("LIFTOFF!!!");
 }
