b84b078709105ac7439da47ed861915056d5f021
robby <robbystk@gmail.com>
2017-07-29 15:26:15 -0600

    conditional loops with `while`

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 fn main() {
-    loop {
-        println!("again!");
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+
+        number = number - 1;
     }
+
+    println!("LIFTOFF!!!");
 }
