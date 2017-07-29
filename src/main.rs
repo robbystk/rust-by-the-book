b0e81cc3baead8fb0462ac01f581c4229f809872
robby <robbystk@gmail.com>
2017-07-29 14:52:32 -0600

    If else arms must be typed compatibly with let

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     let condition = true;
     let number = if condition {
-        5   // note lack of semicolons
+        5
     } else {
-        6
+        "six"
     };
 
     println!("The value of number is: {}", number);
