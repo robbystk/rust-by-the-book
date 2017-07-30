8653d5b89404e15e34872723fd311e132ecadf0d
robby <robbystk@gmail.com>
2017-07-30 12:34:54 -0600

    Write fibonacci program

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,32 @@
+use std::io;
+
 fn main() {
-    // count down from 4
-    // the rev() method reverses the range
-    for number in (1..4).rev() { 
-        println!("{}!", number);
+    println!("Enter number");
+
+    let mut number = String::new();
+
+    io::stdin().read_line(&mut number)
+        .expect("Failed to read line");
+
+    let number: u64 = match number.trim().parse() {
+        Ok(num) => num,
+        Err(_)  => {
+            println!("Please type an integer");
+            return;
+        },
+    };
+
+    println!("The {}th Fibonacci number is {}.", number, fib(number));
+}
+
+fn fib(n: u64) -> u64 {
+    let mut next: u64;
+    let mut current = 1u64;
+    let mut previous = 0u64;
+    for i in 1..n {
+        next = current + previous;
+        previous = current;
+        current = next;
     }
-    println!("LIFTOFF!!!");
+    current
 }
