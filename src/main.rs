4c1fc01dde9281580d0d2619bfc416370c2738fc
robby <robbystk@gmail.com>
2017-07-30 13:34:18 -0600

    create philosopher structs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
-fn main() {
-    // count down from 4
-    // the rev() method reverses the range
-    for number in (1..4).rev() { 
-        println!("{}!", number);
+struct Philosopher {
+    name: String,
+}
+
+impl Philosopher {
+    fn new(name: &str) -> Philosopher {
+        Philosopher {
+            name: name.to_string(),
+        }
     }
-    println!("LIFTOFF!!!");
+}
+
+fn main() {
+    let p1 = Philosopher::new("Baruch Spinoza");
+    let p2 = Philosopher::new("Gilles Deleuze");
+    let p3 = Philosopher::new("Karl Marx");
+    let p4 = Philosopher::new("Friedrich Nietzsche");
+    let p5 = Philosopher::new("Michel Foucault");
 }
