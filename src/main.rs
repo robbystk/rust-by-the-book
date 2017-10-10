d8afae48c3b030e56b5f7b9cf20c54f4eb2bdafb
robby <robbystk@gmail.com>
2017-10-10 14:58:34 -0600

    Print the twelve days of Christmas

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,40 @@
 fn main() {
-    // count down from 4
-    // the rev() method reverses the range
-    for number in (1..4).rev() { 
-        println!("{}!", number);
+    let gifts = [
+        "a partridge in a pair tree",
+        "two turtle doves",
+        "three french hens",
+        "four calling birds",
+        "five golden rings",
+        "six geese a-laying",
+        "seven swans a-swimming",
+        "eight maids a-milking",
+        "nine ladies dancing",
+        "ten lords a-leaping",
+        "eleven pipers piping",
+        "twelve drummers drumming"
+    ];
+
+    let ordinals = [
+        "first",
+        "second",
+        "third",
+        "fourth",
+        "fifth",
+        "sixth",
+        "seventh",
+        "eighth",
+        "ninth",
+        "tenth",
+        "eleventh",
+        "twelfth",
+    ];
+
+    for i in 0..12 {
+            println!("On the {} day of Christmas, my true love gave to me",
+                     ordinals[i]);
+        for g in (0..i+1).rev() {
+            println!("{}", gifts[g]);
+        }
     }
-    println!("LIFTOFF!!!");
+
 }
