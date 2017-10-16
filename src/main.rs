f7991d9c854cfbbb0f9558de1e3f3c916d6dc5f1
robby <robbystk@gmail.com>
2017-10-15 20:28:58 -0600

    Make a String

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 fn main() {
-    // count down from 4
-    // the rev() method reverses the range
-    for number in (1..4).rev() { 
-        println!("{}!", number);
-    }
-    println!("LIFTOFF!!!");
+    let s = String::from("Hello");
+
+    println!("{}, world!", s);
 }
