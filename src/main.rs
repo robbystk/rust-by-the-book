2e5d1d5f326dbc63159b3859be658702eca1c78a
robby <robbystk@gmail.com>
2017-10-15 20:59:13 -0600

    Can't mutably borrow more than once

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
 fn main() {
     let mut s = String::from("hello");
 
-    change(&mut s);
-
-    println!("{}", s);
-}
-
-fn change(some_string: &mut String) {
-    some_string.push_str(", world");
+    let r1 = &mut s;
+    let r2 = &mut s;
 }
