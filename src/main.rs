c7d3e5349d1714683986e22f84ab594c3c5682fb
robby <robbystk@gmail.com>
2018-02-01 12:44:16 -0700

    Second word function is harder than I expected

--- src/main.rs
+++ src/main.rs
@@ -21,3 +21,24 @@ fn first_word(s: &String) -> &str {
 
     &s[..] // or entire string if no spaces (==> one word)
 }
+
+fn second_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    let mut start_found = false;
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            if start_found {
+                let stop = i;
+                return &s[start..stop];
+            } 
+            else {
+                let start = i;
+                start_found = true;
+            }
+        }
+    }
+
+    &s[..]
+}
