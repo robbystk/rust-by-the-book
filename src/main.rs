3158d2ba258f54eb06cf99407ae87e73daec7eb2
robby <robbystk@gmail.com>
2018-02-01 15:33:21 -0700

    Structs can't reference without lifetimes (nocompile)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 fn main() {
-    let black = Color(0,0,0);
-    let origin = Point(0,0,0);
+    let user1 = User {
+        email: "someone@example.com",
+        username: "somebody123",
+        active: true,
+        sign_in_count: 1,
+    };
 }
 
-struct Color(i32,i32,i32);
-struct Point(i32,i32,i32);
+struct User {
+    username: &str,
+    email: &str,
+    sign_in_count: u64,
+    active: bool,
+}
