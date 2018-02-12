62fdc0b842307fe592e8a7c91f8f21ac6ee53066
robby <robbystk@gmail.com>
2018-02-12 08:22:37 -0700

    Use asociated functions with `::` syntax

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,9 @@ fn main() {
     let rect1 = Rectangle { width:30, height: 50 };
     let rect2 = Rectangle { width:10, height: 40 };
     let rect3 = Rectangle { width:60, height: 45 };
+    let sq = Rectangle::square(20);
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    println!("Can rect1 hold sq? {}", rect1.can_hold(&sq));
 }
