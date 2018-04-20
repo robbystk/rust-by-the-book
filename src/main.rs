a20764fdc48145bbf35d5ab643ada14f7ecacf31
robby <robbystk@gmail.com>
2018-04-20 15:01:14 -0600

    Using match with Option<T>

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,12 @@
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter(UsState),
-}
-
-#[derive(Debug)]
-enum UsState {
-    Alabama,
-    Alaska,
-    Arizona,
-    Arkansas,
-    California,
-    Colorado,
-    // etc
-}
-
-fn value_in_cents(coin: Coin) -> i32 {
-    match coin {
-        Coin::Penny => {
-            println!("Lucky penny!");
-            1
-        },
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter(state) => {
-            println!("{:?} state quarter!", state);
-            25
-        },
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        Some(i) => Some(i + 1),
+        None => None,
     }
 }
 
 fn main() {
-    let coin = Coin::Quarter(UsState::Colorado);
-
-    println!("The coin has a value of {} cents",
-             value_in_cents(coin));
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
 }
