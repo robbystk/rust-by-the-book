75306a0cf5850039c7e46dfe3a9a581eeddff5e2
robby <robbystk@gmail.com>
2018-04-20 15:08:27 -0600

    Match for only a single case is excessively wordy

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 fn main() {
-    let some_u8_value = 7u8;
+    let some_u8_value = Some(7u8);
     match some_u8_value {
-        1 => println!("one"),
-        3 => println!("three"),
-        5 => println!("five"),
-        7 => println!("seven"),
+        Some(3) => println!("three"),
         _ => (),
     }
 }
