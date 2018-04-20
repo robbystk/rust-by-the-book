434eab436fd1d7ecfe927fe9d53a1592617d995a
robby <robbystk@gmail.com>
2018-04-20 14:14:56 -0600

    Can define methods on enums, just like structs

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,16 @@ enum Message {
     ChangeColor(i32, i32, i32)
 }
 
+impl Message {
+    fn call(&self) {
+        // pass
+    }
+}
+
 
 fn main() {
+    let m = Message::Write(String::from("hello"));
+
+    m.call();
 
 }
