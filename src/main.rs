530af4d9cb2146fe1928a8a32ed9385212a90be3
robby <robbystk@gmail.com>
2018-04-20 14:07:59 -0600

    use string inside variants for conciseness

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,14 @@
-enum IpAddrKind {
-    V4,
-    V6,
-}
-
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+enum IpAddr {
+    V4(String),
+    V6(String),
 }
 
 fn route(ip: IpAddr) { }
 
 fn main() {
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
+    let home = IpAddr::V4(String::from("127.0.0.1"));
 
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
+    let loopback = IpAddr::V6(String::from("::1"));
 
     route(home)
 }
