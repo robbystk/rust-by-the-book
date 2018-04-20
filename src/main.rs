2768b692cc9bbd880fdad9779f3b5e02b4d9eb23
robby <robbystk@gmail.com>
2018-04-20 15:10:25 -0600

    Use if let instead of match for single cases

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 fn main() {
     let some_u8_value = Some(7u8);
-    match some_u8_value {
-        Some(3) => println!("three"),
-        _ => (),
+    if let Some(3) =  some_u8_value {
+        println!("three");
     }
 }
