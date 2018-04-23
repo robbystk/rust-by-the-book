2d19a21800273566ba1cacc398e334320d1805d5
robby <robbystk@gmail.com>
2018-04-23 15:33:22 -0600

    type annotations

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,6 @@
-use std::io;
-
 fn main() {
-    let x = 5;
-    println!("The value of x is: {}", x);
-    let x = x + 1;
-    println!("The value of x is: {}", x);
-    let x = x * 2;
-    println!("The value of x is: {}", x);
+    let guess: u32 = "42".parse().expect("Not a number!");
 
-    // Get a number of spaces from user
-    println!("Enter your desired number of spaces");
-    let mut spaces = String::new();
-    io::stdin().read_line(&mut spaces)
-        .expect("Failed to read line");
-
-    let spaces = spaces.len();
-
-    println!("Number of spaces: {}", spaces);
+    // compile-time error:
+    // let guess = "42".parse().expect("Not a number!");
 }
