15f1ff42f7580cb6ded5392890c975cb282620bc
robby <robbystk@gmail.com>
2018-04-23 15:00:11 -0600

    Revert back to state quarter example
    This reverts commits 6f33db38482d7d2aa9aa71f33d102f7e5fa0d38b,
                         83302f08b6eb48dd0b8dbe28a20d4024e81a1ffb,
                         8795b979abaf1791b487f2f830494122192593f0,
                         297e20333a19594b87216916e313648619c082d3,
                         ea459f8821a41cd282650db838f2de2d0691ce0a.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,39 @@
-fn main() {
-    let some_u8_value = Some(7u8);
-    if let Some(3) =  some_u8_value {
-        println!("three");
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    Arizona,
+    Arkansas,
+    California,
+    Colorado,
+    // etc
+}
+
+fn value_in_cents(coin: Coin) -> i32 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("{:?} state quarter!", state);
+            25
+        },
     }
+}
+
+fn main() {
+    let coin = Coin::Quarter(UsState::Colorado);
+
+    println!("The coin has a value of {} cents",
+             value_in_cents(coin));
 }
