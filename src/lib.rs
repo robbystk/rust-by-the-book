ef3eecb1c285c3f21ae841f933db16bb870c1341
robby <robbystk@gmail.com>
2018-05-17 08:30:09 -0600

    What if outer is public but inner is private?

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-mod outermost {
+pub mod outermost {
     pub fn middle_function() {}
 
     fn middle_secret_function() {}
 
-    pub mod inside {
+    mod inside {
         pub fn inner_function() {}
 
         fn secret_function() {}
@@ -12,9 +12,9 @@ mod outermost {
 
 fn try_me() {
     outermost::middle_function();   // I think this will work
-    outermost::middle_secret_function();    // this will not
-    // middle_secret_function can be used by outermost, but we are
+    outermost::middle_secret_function();    // this will too (wrong)
+    // middle_secret_function can still be used by outermost, but we are
     // outside of outermost and cannot use it.
-    outermost::inside::inner_function();    // this will now
+    outermost::inside::inner_function();    // this will not
     outermost::inside::secret_function();   // nor this
 }
