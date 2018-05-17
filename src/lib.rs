ee6b7f90b1e51fde1a2fe28cc7e3df8b6954da3f
robby <robbystk@gmail.com>
2018-05-17 08:34:40 -0600

    Try calling middle secret function from other function

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,16 @@
-pub mod outermost {
+mod outermost {
     pub fn middle_function() {}
 
     fn middle_secret_function() {}
 
-    mod inside {
-        pub fn inner_function() {}
+    // let's make this public so it inner_function can be called
+    pub mod inside {
+        pub fn inner_function() {
+            // I think this will work because we are in a child of outermost and
+            // middle_secret_function can be accessed from outermost.
+            // But there might be shenanigans because we went to the root (::)
+            ::outermost::middle_secret_function();
+        }
 
         fn secret_function() {}
     }
@@ -12,9 +18,9 @@ pub mod outermost {
 
 fn try_me() {
     outermost::middle_function();   // I think this will work
-    outermost::middle_secret_function();    // this will too (wrong)
-    // middle_secret_function can still be used by outermost, but we are
+    outermost::middle_secret_function();    // this will not
+    // middle_secret_function can be used by outermost, but we are
     // outside of outermost and cannot use it.
-    outermost::inside::inner_function();    // this will not
-    outermost::inside::secret_function();   // nor this
+    outermost::inside::inner_function();    // this will now work
+    outermost::inside::secret_function();   // this will not
 }
