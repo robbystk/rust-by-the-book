24773c8776eb368955baabd8504aab809d35d360
robby <robbystk@gmail.com>
2018-05-29 10:42:00 -0600

    Use super to access adjacent children

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,10 @@ pub mod client;
 
 #[cfg(test)]
 mod tests {
+    use super::client;
+
     #[test]
     fn it_works() {
-        client::connect()   // Will not work
+        client::connect();   // Will work
     }
 }
