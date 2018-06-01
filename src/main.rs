d27b2909da644a2e0a976ae8b1c7cfe27d8867dc
robby <robbystk@gmail.com>
2018-06-01 08:35:18 -0600

    Accessing elements with indexing or get() method

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 fn main() {
-    let mut v = Vec::new();
+    let v = vec![1, 2, 3, 4, 5];
 
-    v.push(5);
-    v.push(6);
-    v.push(7);
-    v.push(8);
+    let third: &i32 = &v[2];
+    let third: Option<&i32> = v.get(2);
 }
