69c795b355694fd59b1c14ea97bc9ce18fb1a280
robby <robbystk@gmail.com>
2018-06-03 08:55:19 -0600

    Use for to iterate over vector elements

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    let mut v = vec![1, 2, 3, 4, 5];
+    let v = vec![1, 2, 3, 4, 5];
 
-    let third: &i32 = &v[2];  // borrow reference to third element
-
-    v.push(6);
+    for i in &v {
+        println!("{}", i);
+    }
 }
