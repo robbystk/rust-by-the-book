f063eb2a1c9e0da71d1a1ce250f84aba8ffeada0
robby <robbystk@gmail.com>
2018-06-04 07:59:04 -0600

    Use an enum to produce inhomogenous vectors

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 fn main() {
-    let mut v = vec![1, 2, 3, 4, 5];
 
-    for i in &mut v {
-        *i += 50;
+    #[derive(Debug)]
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
     }
 
-    println!("{:?}", v);
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+
+    println!("{:?}", row);
 }
