cd59b6f36dfd56ebe2499a1fd09c7162906b11e3
robby <robbystk@gmail.com>
2018-06-05 08:32:08 -0600

    Internationalized hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    let s = String::from("initial contents");
+    let hello = String::from("Dobrý den");
+    let hello = String::from("Hello");
+    let hello = String::from("םולש");
+    let hello = String::from("こんにちわ");
+    let hello = String::from("Olá");
+    let hello = String::from("Здравствуйте");
+    println!("{} world!", hello);
 }
