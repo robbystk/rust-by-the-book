18b6118d73f4c830e432e15980b4850ea8d31321
robby <robbystk@gmail.com>
2018-06-05 08:55:48 -0600

    Can also concatenate using + (surprisingly complex)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
 fn main() {
-    let mut s = String::from("lo");
+    let s1 = String::from("Hello,");
+    let s2 = String::from(" world!");
 
-    s.push('l');
+    let s3 = s1 + &s2;  // s1 gets moved here (to s3, kind of)
+    // s2 must be borrowed to avoid taking ownership of it and because that's
+    // what the add() function expects.
 
-    println!("{}", s);
+    // The function signature of String::add() is approximately
+    // fn add(self, s: &str) -> String {
+
+    // Note even though the signature asks for a &str, we give it an &String
+    // which gets automatically coerced into a string slice.
+
+    // so we can no longer use s1 (but can still use s2)
+    // println!("{}", s1);
+
+    println!("{}", s3);
 }
