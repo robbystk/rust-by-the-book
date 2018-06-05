d8a0c9825de0d46796f616ae04b55dd1f599b7f1
robby <robbystk@gmail.com>
2018-06-05 08:48:04 -0600

    Can add to a string with the push_str() method

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 fn main() {
-    let hello = String::from("Dobrý den");
-    let hello = String::from("Hello");
-    let hello = String::from("םולש");
-    let hello = String::from("こんにちわ");
-    let hello = String::from("Olá");
-    let hello = String::from("Здравствуйте");
-    println!("{} world!", hello);
+    let mut s = String::from("foo");
+
+    s.push_str("bar");
+
+    println!("{}", s);
 }
