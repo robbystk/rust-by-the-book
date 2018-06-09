21d117f938086aace630cac5633db3ceb806eb99
robby <robbystk@gmail.com>
2018-06-09 08:37:05 -0600

    Using + for string concatenation gets complicated

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,9 @@
 fn main() {
-    let s1 = String::from("Hello,");
-    let s2 = String::from(" world!");
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
 
-    let s3 = s1 + &s2;  // s1 gets moved here (to s3, kind of)
-    // s2 must be borrowed to avoid taking ownership of it and because that's
-    // what the add() function expects.
+    let s = s1 + "-" + &s2 + "-" + &s3;
 
-    // The function signature of String::add() is approximately
-    // fn add(self, s: &str) -> String {
-
-    // Note even though the signature asks for a &str, we give it an &String
-    // which gets automatically coerced into a string slice.
-
-    // so we can no longer use s1 (but can still use s2)
-    // println!("{}", s1);
-
-    println!("{}", s3);
+    println!("{}", s);
 }
