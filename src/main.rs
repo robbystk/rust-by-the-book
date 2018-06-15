27b89a313075dc16c468ba8797a14926cdb8d431
robby <robbystk@gmail.com>
2018-06-15 12:50:22 -0600

    Construct hash map from vectors using collect

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::collections::HashMap;
 
 fn main() {
-    let mut scores = HashMap::new();
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
 
-    scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
+    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
 }
