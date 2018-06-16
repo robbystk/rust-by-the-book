94d4936c01062b557a95b9333bc7c4ea60ac370e
robby <robbystk@gmail.com>
2018-06-16 07:49:24 -0600

    Keys and values get moved into hash map (nocompile)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::collections::HashMap;
 
 fn main() {
-    let teams = vec![String::from("Blue"), String::from("Yellow")];
-    let initial_scores = vec![10, 50];
+    let field_name = String::from("My favorite color");
+    let field_value = String::from("Blue");
 
-    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+
+    println!("{}", field_name); // Can't because it's owned by map now.
 }
