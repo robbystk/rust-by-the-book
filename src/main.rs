801413186b861ea98d8e9ef08142efe8a23e20d0
robby <robbystk@gmail.com>
2018-06-20 20:10:29 -0600

    Sort, compute frequencies and mean

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,33 @@
 use std::collections::HashMap;
 
 fn main() {
-    let text = "hello world wonderful world";
+    let mut list = [1, 5, 6, 4, 8, 9, 10, 3, 11, 2, 1, 1, 9, 5, 4, 5];
 
-    let mut map = HashMap::new();
+    // we do not give a shit about stability, and unstable sorting is faster and
+    // uses less memory.
+    list.sort_unstable();
 
-    for word in text.split_whitespace() {
-        let count = map.entry(word).or_insert(0);
+    // hash map to store values as frequencies
+    let mut frequencies = HashMap::new();
+
+    for number in list.iter() {
+        let count = frequencies.entry(number).or_insert(0);
         *count += 1;
     };
 
-    for (key, value) in map.iter() {
+    for (key, value) in frequencies.iter() {
         println!("{}: {}", key, value);
     }
+
+    // compute mean
+    let mut sum = 0f32;
+    let mut count = 0f32;
+    for (number, frequency) in frequencies.iter() {
+        sum += (*number * *frequency) as f32;
+        count += *frequency as f32;
+    }
+
+    let mean = sum / count;
+
+    println!("The mean is {}.", mean);
 }
