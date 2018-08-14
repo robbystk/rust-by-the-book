e62eca2dd442a733aa25e8f1c63ea614a506b664
Robby Stokoe <robbystk@gmail.com>
2018-08-13 21:53:45 -0600

    `unwrap()` panics on `Err()`

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,5 @@
 use std::fs::File;
-use std::io::ErrorKind;
 
 fn main() {
-    let f = match File::open("hello.txt") {
-        Ok(file) => file,
-        Err(ref error) if error.kind() == ErrorKind::NotFound => {
-            match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => {
-                    panic!(
-                        "Tried to create file but there was a problem: {:?}",
-                        e
-                        )
-                }
-            }
-        },
-        Err(error) => {
-            panic!("There was a problem opening the file: {:?}", error);
-        },
-    };
+    let f = File::open("hello.txt").unwrap();
 }
