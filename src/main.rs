9e21ed7d2dc643dab1ec37b80814244302603251
Robby Stokoe <robbystk@gmail.com>
2018-08-13 21:45:44 -0600

    Inline `File::open()`

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,7 @@ use std::fs::File;
 use std::io::ErrorKind;
 
 fn main() {
-    let f = File::open("hello.txt");
-
-    let f = match f {
+    let f = match File::open("hello.txt") {
         Ok(file) => file,
         Err(ref error) if error.kind() == ErrorKind::NotFound => {
             match File::create("hello.txt") {
