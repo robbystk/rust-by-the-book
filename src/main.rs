90d7cdbbe373eff0757cedf9336e14e23a068bd0
Robby Stokoe <robbystk@gmail.com>
2018-08-13 21:16:33 -0600

    The `panic!()` macro

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,3 @@
-use std::collections::HashMap;
-
 fn main() {
-    let text = "hello world wonderful world";
-
-    let mut map = HashMap::new();
-
-    for word in text.split_whitespace() {
-        let count = map.entry(word).or_insert(0);
-        *count += 1;
-    };
-
-    for (key, value) in map.iter() {
-        println!("{}: {}", key, value);
-    }
+    panic!("crash and burn");
 }
