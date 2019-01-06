e3467eac6f6d77f0e3d1243a168375b38475bd52
Robby Stokoe <robbystk@gmail.com>
2019-01-06 12:20:22 -0700

    Find largest number in the list

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
-use std::fs::File;
-
 fn main() {
-    let f = File::open("hello.txt").expect("Failed to open hello.txt");
+    let number_array = vec![34, 50, 25, 100, 65];
+
+    let mut largest = number_array[0];
+
+    for n in number_array {
+        if n > largest {
+            largest = n;
+        }
+    }
+
+    println!("The largest number is {}", largest)
 }
