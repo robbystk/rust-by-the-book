b24e932b30796811aad52f49654b3f7bbf1c7f7a
Robby Stokoe <robbystk@gmail.com>
2019-01-06 13:05:02 -0700

    Lifetimes on structs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
-// Rust can't tell whether the returned reference will be from s1 or s2 (and
-// neither can we), so we have to specify the lifetime
-fn longest<'a>(s1: &'a str, s2: &'a str) -> &'a str {
-    if s1.len() > s2.len() {
-        s1
-    } else {
-        s2
-    }
+struct ImportantExcerpt<'a> {
+    part: &'a str,
 }
 
 fn main() {
-    let string1 = String::from("abcd");
-    let string2 = "xyz";
-
-    let result = longest(&string1, string2);
-    println!("The longest string is {}", result);
+    let novel = String::from("Call me Ishmael.  Some years ago . . . ");
+    let first_sentence = novel.split('.')
+        .next()
+        .expect("Could not find a '.'");
+    let i = ImportantExcerpt { part: &first_sentence };
 }
