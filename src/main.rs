4f38aafbad35abea7ae44f499a15f5f3df07f471
Robby Stokoe <robbystk@gmail.com>
2019-01-06 12:29:02 -0700

    Functionalize for reuse

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
-fn main() {
-    let number_array = vec![34, 50, 25, 100, 65];
+fn largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
 
-    let mut largest = number_array[0];
-
-    for n in number_array {
+    for &n in list {
         if n > largest {
             largest = n;
         }
     }
 
-    println!("The largest number is {}", largest)
+    largest
+}
+
+fn main() {
+    let number_array = vec![34, 50, 25, 100, 65];
+
+    println!("The largest number is {}", largest(&number_array));
+
+    let number_array = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    println!("The largest number is {}", largest(&number_array));
 }
