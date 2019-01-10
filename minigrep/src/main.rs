7c7ad5b5f82b8b51b75eca053b8dc760cf24ddf9
Robby Stokoe <robbystk@gmail.com>
2019-01-09 19:42:43 -0700

    Move main logic out of main and handle errors

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,22 +1,31 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
-        process::exit(1)
+        process::exit(1);
     });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong while reading the file");
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
 
     println!("With text:\n{}", contents);
+
+    Ok(())
 }
 
 struct Config<'a> {
