93ddd7eebfaa07ee72af674bfa6248c4f654dc68
Robby Stokoe <robbystk@gmail.com>
2019-01-09 19:26:02 -0700

    Wrap configuration in a struct

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,19 +4,24 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong while reading the file");
 
     println!("With text:\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
+struct Config<'a> {
+    query: &'a str,
+    filename: &'a str,
+}
+
+fn parse_config(args: &[String]) -> Config {
     let query = &args[1];
     let filename = &args[2];
-    (query, filename)
+    Config { query, filename }
 }
