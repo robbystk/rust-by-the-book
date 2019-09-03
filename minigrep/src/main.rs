dbfa61293b26ee85381437348855ae5726ebe088
Robby Stokoe <robbystk@gmail.com>
2019-09-02 20:36:41 -0600

    Use a constructor for the configuration

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file);
@@ -20,8 +20,10 @@ struct Config {
     file: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file = args[2].clone();
-    Config {query, file,}
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file = args[2].clone();
+        Config {query, file,}
+    }
 }
