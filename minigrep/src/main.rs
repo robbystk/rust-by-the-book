8f2f94a83d297a587219f6f42feef63cc17c8405
Robby Stokoe <robbystk@gmail.com>
2019-09-02 20:35:11 -0600

    Package configuration into a struct

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,19 +4,24 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file);
 
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.file)
         .expect("Something went wrong while reading the file");
 
     println!("With text:\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file = &args[2];
-    (query, file)
+struct Config {
+    query: String,
+    file: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file = args[2].clone();
+    Config {query, file,}
 }
