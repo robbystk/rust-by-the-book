a5d56ced2ea0ba57e090989c5cea73e929174408
Robby Stokoe <robbystk@gmail.com>
2019-09-03 21:14:35 -0600

    Cache the result of the expensive calculation

--- functional/src/main.rs
+++ functional/src/main.rs
@@ -8,14 +8,16 @@ fn simulated_expensive_calculation(intensity: u32) -> u32 {
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
+    let mut expensive_result = Cacher::new(simulated_expensive_calculation);
+
     if intensity < 25 {
         println!(
             "Today, do {} pushups!",
-            simulated_expensive_calculation(intensity),
+            expensive_result.value(intensity),
         );
         println!(
             "Next, do {} situps!",
-            simulated_expensive_calculation(intensity),
+            expensive_result.value(intensity),
         );
     } else {
         if random_number == 3 {
@@ -23,8 +25,37 @@ fn generate_workout(intensity: u32, random_number: u32) {
         } else {
             println!(
                 "Today, run for {} minutes!",
-                simulated_expensive_calculation(intensity),
+                expensive_result.value(intensity),
             );
+        }
+    }
+}
+
+struct Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    function: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    fn new(func: T) -> Cacher<T> {
+        Cacher {
+            function: func,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.function)(arg);
+                self.value = Some(v);
+                v
+            },
         }
     }
 }
